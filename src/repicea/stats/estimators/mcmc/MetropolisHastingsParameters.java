81c5c307ce40ea66dcc3f6fb94f4b436a3d8853d
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2024-03-12 11:40:55 -0400

    Improving documentation Adding a method to set priors in MetropolisHastingsCompatibleModel interface Correcting headers

--- src/repicea/stats/estimators/mcmc/MetropolisHastingsParameters.java
+++ src/repicea/stats/estimators/mcmc/MetropolisHastingsParameters.java
@@ -1,7 +1,8 @@
 /*
- * This file is part of the repicea library.
+ * This file is part of the repicea-mathstats library.
  *
- * Copyright (C) 2009-2021 Mathieu Fortin for Rouge Epicea.
+ * Copyright (C) 2021-24 His Majesty the King in Right of Canada
+ * Author: Mathieu Fortin, Canadian Forest Service
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,14 +19,49 @@
  */
 package repicea.stats.estimators.mcmc;
 
+/**
+ * A class containing the simulation parameters for the MCMC algorithm.
+ * @author Mathieu Fortin - September 2021
+ */
 public class MetropolisHastingsParameters {
 
+	/**
+	 * The number of samples in the burn-in period.<p>
+	 * Is normally set to 10000.
+	 */
 	public int nbBurnIn = 10000;
-	public int nbRealizations = 500000 + nbBurnIn;
+	
+	/**
+	 * Total number of accepted realizations in the Markov Chain.<p>
+	 * Is normally set to 500000 + {@link MetropolisHastingsParameters#nbBurnIn}. 
+	 */
+	public int nbAcceptedRealizations = 500000 + nbBurnIn;
+	
+	/**
+	 * The number of internal realizations.<p>
+	 * The algorithm will stop if no realization has been accepted in the chain after this number of realizations. 
+	 * Is normally set to 100000.
+	 */
 	public int nbInternalIter = 100000;
+	
+	/**
+	 * The selection rate to obtain the final sample.<p>
+	 * Once the chain contains the appropriate number of realizations (see {@link MetropolisHastingsParameters#nbAcceptedRealizations}),
+	 * then those of the burn-in period are discarded and one realization every x realizations is selected to the final sample. This rate 
+	 * is set through this parameter. Is normally set to 50.
+	 */
 	public int oneEach = 50;
+	
+	/**
+	 * Set the size of the initial grid.<p>
+	 * The realizations are generated at random and that with the greatest likelihood is selected as
+	 * starting value for the Markov Chain.
+	 */
 	public int nbInitialGrid = 10000;	
 
+	/**
+	 * An empty constructor with default values.
+	 */
 	public MetropolisHastingsParameters() {}
 
 	@Override
